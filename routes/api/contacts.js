60e248cd9202e5c4b2f6a510f55badb3a1e8413c
DariaDrozd <reddymargot@gmail.com>
2023-08-10 14:08:30 +0300

    Home W_ordk_doen_test

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'Home work Done_Test' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
